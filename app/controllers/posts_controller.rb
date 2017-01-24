01bca1386a1543b976bed7decccb0a24061553cd
David Pee <david_tahwei@hotmail.com>
2017-01-25 00:22:31 +0800

    added form for post and create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
 
-def index
-end
+	def index
+	end
 
+	def new  
+	  @post = Post.new
+	end 
+
+	def create
+		@post = current_user.posts.new(post_params)
+		if @post.save
+			redirect_to posts_path
+		end
+	end
+
+	def post_params  
+  	params.require(:post).permit(:image, :caption)
+	end 
 end
