700c9a8ca6edae19941e5de59dd799ace124b56a
David Pee <david_tahwei@hotmail.com>
2017-01-24 23:41:36 +0800

    Post model done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+	validates :image, presence: true
+
+	#re-sizing the image - Instagram 640px wide, tall images is fine
+	has_attached_file :image, styles: { :medium => "640x"}
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
